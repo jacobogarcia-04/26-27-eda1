41e4133acbabc67852160b82920fb793f62c9636
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-09-19 13:51:20 +0200

    feet: se añaden metodos necesarios para la clase tiempo

--- entregas/garcíaJacobo/Tiempo.java
+++ entregas/garcíaJacobo/Tiempo.java
@@ -1,3 +1,27 @@
 public class Tiempo {
-    
+    private final double HORA_APERTURA = 10.0;
+    private final double HORA_CIERRE = 14.0;
+    private final double MINUTO = 0.0167;
+    private double horaActual;
+    private Console console;
+
+    public Tiempo() {
+        horaActual = HORA_APERTURA;
+        console = new Console();
+    }
+
+    public void avanzar() {
+
+        horaActual = horaActual + MINUTO;
+    }
+
+    public boolean haFinalizado() {
+        return horaActual >= HORA_CIERRE;
+    }
+
+    private String horaHumana() {
+        int hora = (int) horaActual;
+        int minutos = (int) (horaActual - hora) * 60;
+        return hora + ":" + minutos;
+    }
 }
