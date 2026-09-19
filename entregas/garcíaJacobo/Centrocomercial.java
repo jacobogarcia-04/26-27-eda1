3b2ebe5a73885fb7c49f44d6ebcf56e7e99fc345
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-09-19 14:12:11 +0200

    feet: se crean el flujo del programa en la clase CentroComercial

--- entregas/garcíaJacobo/Centrocomercial.java
+++ entregas/garcíaJacobo/Centrocomercial.java
@@ -1,12 +1,62 @@
 public class CentroComercial {
-    private final double PROBABILIDAD_LLEGADA_CLIENTES = 0.6;
+
     private Fila fila;
     private Tiempo tiempo;
     private Caja[] cajas;
+    private Console console;
     private boolean haLlegadoCliente;
+    private final double PROBABILIDAD_LLEGADA_CLIENTES = 0.6;
+    private final double PROBABILIDAD_CAJA_LIBRE = 0.4;
+    private final int NUMERO_CAJAS = 4;
+
+    public CentroComercial() {
+        fila = new Fila();
+        cajas = new Caja[NUMERO_CAJAS];
+        tiempo = new Tiempo();
+        console = new Console();
+        for (int i = 0; i <= NUMERO_CAJAS; i++) {
+            cajas[i] = new Caja();
+        }
+    }
 
     public void ejecutar() {
+        do {
 
+            tiempo.avanzar();
+            this.procesarLlegadaCliente();
+            this.registrarEstado();
+            this.asignarClienteACaja();
+            this.atenderCliente();
+            this.registrarEstado();
+            this.pausar();
+
+        } while (!tiempo.haFinalizado());
+
+    }
+
+    private void pausar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pausar'");
+    }
+
+    private void atenderCliente() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atenderCliente'");
+    }
+
+    private void asignarClienteACaja() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'asignarClienteACaja'");
+    }
+
+    private void registrarEstado() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'registrarEstado'");
+    }
+
+    private void procesarLlegadaCliente() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'procesarLlegadaCliente'");
     }
 
 }
