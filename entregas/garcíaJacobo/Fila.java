8587908f7c852964f9961011b9e756126b16fb4d
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-09-19 18:52:32 +0200

    feet: se agregan la logica de los metodos de fila

--- entregas/garcíaJacobo/Fila.java
+++ entregas/garcíaJacobo/Fila.java
@@ -1,33 +1,58 @@
 public class Fila {
     private Cliente[] clientes;
     private int numeroClientes;
+    private int[] estados;
+    private int numeroEstados;
+    private Console console;
 
-    public Object primero() {
+    public Fila() {
+        console = new Console();
+        estados = new int[240];
+        numeroEstados = 0;
+        clientes = new Cliente[1000];
+    }
 
+    public Cliente primero() {
+        return clientes[0];
     }
 
     public boolean hayGente() {
-
+        return numeroClientes > 0;
     }
 
     public Cliente sacar() {
+        Cliente cliente = clientes[0];
+        for (int i = 1; i < numeroClientes; i++) {
+            clientes[i - 1] = clientes[i];
 
+        }
+        numeroClientes--;
+        return cliente;
     }
 
     public void añadirCliente(Cliente cliente) {
-
+        clientes[numeroClientes] = cliente;
+        numeroClientes++;
     }
 
-    public void mostrar() {
+   public void mostrar() {
+    console.writeln("FILA:");
 
+    if (numeroClientes == 0) {
+        console.writeln("  Vacia");
+    } else {
+        for (int i = 0; i < numeroClientes; i++) {
+            console.writeln("  Cliente " + (i + 1));
+        }
     }
-
+}
     public void registrarEstado() {
-
+        estados[numeroEstados] = numeroClientes;
+        numeroEstados++;
     }
 
     public int obtenerNumero() {
-
+        return numeroClientes;
     }
 
 }
