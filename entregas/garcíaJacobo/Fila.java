8bfebd83c721ea94f9ffbf90abcf3afc098ae63f
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-09-19 18:01:00 +0200

    Feet: se añaden atributos de la clase fila

--- entregas/garcíaJacobo/Fila.java
+++ entregas/garcíaJacobo/Fila.java
@@ -1,37 +1,33 @@
 public class Fila {
+    private Cliente[] clientes;
+    private int numeroClientes;
 
     public Object primero() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'primero'");
+
     }
 
     public boolean hayGente() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hayGente'");
+
     }
 
     public Cliente sacar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sacar'");
+
     }
 
     public void añadirCliente(Cliente cliente) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'añadirCliente'");
+
     }
 
- 
-
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+
     }
 
     public void registrarEstado() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'registrarEstado'");
+
     }
 
- 
-    
+    public int obtenerNumero() {
+
+    }
+
 }
