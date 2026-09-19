a3620a1681d747c3437bb220fc832a8d180a64fb
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-09-19 13:52:14 +0200

    feet : se añaden los atributos necesarios de Centrocomercial

--- entregas/garcíaJacobo/Centrocomercial.java
+++ entregas/garcíaJacobo/Centrocomercial.java
@@ -1,4 +1,9 @@
 public class CentroComercial {
+    private final double PROBABILIDAD_LLEGADA_CLIENTES = 0.6;
+    private Fila fila;
+    private Tiempo tiempo;
+    private Caja[] cajas;
+    private boolean haLlegadoCliente;
 
     public void ejecutar() {
 
