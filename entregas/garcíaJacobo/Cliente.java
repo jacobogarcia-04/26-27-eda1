1dbabc819ccd0f6c40c46e8e399f8dcb8fb17c47
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-09-24 16:27:00 +0200

    Feet: Cambios necesarios en Cliente para afrontar el reto sin arrays"

--- entregas/garcíaJacobo/Cliente.java
+++ entregas/garcíaJacobo/Cliente.java
@@ -1,11 +1,21 @@
 public class Cliente {
     private int minutoLlegada;
 
+    private Cliente proximo;
+
     public Cliente(int minutoLlegada) {
         this.minutoLlegada = minutoLlegada;
     }
 
     public int minutosEnCola(int minutoActual) {
         return minutoActual - minutoLlegada;
+    }
+
+    public void proximoCliente(Cliente cliente){
+        proximo=cliente;
+    }
+
+    public Cliente obtenerProximo(){
+        return proximo;
     }
 }
