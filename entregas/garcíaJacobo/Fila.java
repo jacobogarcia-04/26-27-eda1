ea5410a784c71cc873ca28bbc68db8c7500d4cf8
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-09-24 17:34:37 +0200

    refactor: cambios en la fila para poder usarla sin arrays

--- entregas/garcíaJacobo/Fila.java
+++ entregas/garcíaJacobo/Fila.java
@@ -1,74 +1,66 @@
 public class Fila {
-    private Cliente[] clientes;
-    private int numeroClientes;
-    private int[] estados;
-    private int numeroEstados;
-    private final double PROBABILIDAD_ABURRIRSE_CLIENTE = 0.3;
+
+    private Cliente primero;
+    private Cliente ultimo;
     private Console console;
 
     public Fila() {
         console = new Console();
-        estados = new int[240];
-        numeroEstados = 0;
-        clientes = new Cliente[1000];
+
     }
 
     public Cliente primero() {
-        return clientes[0];
+        return primero;
     }
 
     public boolean hayGente() {
-        return numeroClientes > 0;
+        return primero != null;
     }
 
     public Cliente sacar() {
-        Cliente cliente = clientes[0];
-        for (int i = 1; i < numeroClientes; i++) {
-            clientes[i - 1] = clientes[i];
+        Cliente cliente = primero;
+        primero = cliente.obtenerProximo();
 
+        if (primero == null) {
+            ultimo = null;
         }
-        numeroClientes--;
         return cliente;
+
     }
 
     public void añadirCliente(Cliente cliente) {
-        clientes[numeroClientes] = cliente;
-        numeroClientes++;
+        if (!this.hayGente()) {
+            primero = cliente;
+            ultimo = cliente;
+        } else {
+
+            ultimo.proximoCliente(cliente);
+            ultimo = cliente;
+        }
+
     }
 
     public void mostrar() {
         console.writeln("FILA:");
+        Cliente clienteActual = primero;
+        int contador = 0;
+        while (clienteActual != null) {
+            contador++;
+            console.writeln("Cliente" + contador);
+            clienteActual = clienteActual.obtenerProximo();
 
-        if (numeroClientes == 0) {
-            console.writeln("  Vacia");
-        } else {
-            for (int i = 0; i < numeroClientes; i++) {
-                console.writeln("  Cliente " + (i + 1));
-            }
         }
-    }
-
-    public void registrarEstado() {
-        estados[numeroEstados] = numeroClientes;
-        numeroEstados++;
     }
 
     public int obtenerNumero() {
-        return numeroClientes;
-    }
+        int contador = 0;
+        Cliente clienteActual = primero;
 
-    public void comprobarAburrimiento(int minutoActual) {
-        for (int i = 0; i < numeroClientes; i++) {
-            if (clientes[i].minutosEnCola(minutoActual) > 8) {
-                if (Math.random() < PROBABILIDAD_ABURRIRSE_CLIENTE) {
-                    for (int j = i + 1; j < numeroClientes; j++) {
-                        clientes[j - 1] = clientes[j];
-                    }
-
-                    numeroClientes--;
-                }
-            }
+        while (clienteActual != null) {
+            clienteActual = clienteActual.obtenerProximo();
+            contador++;
         }
+        return contador;
     }
 
 }
